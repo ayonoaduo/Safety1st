eef52eb142599c2f4167f369116eaa9fbdf38e05
Jingkang_Yang <yang242j@uregina.ca>
2020-04-06 14:16:17 -0600

    Create Project with App icon

---
+++ APPLICATION/Safety1st/Safety1st/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Safety1st
+//
+//  Created by 杨靖康 on 2020-04-06.
+//  Copyright © 2020 Jingkang Yang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
